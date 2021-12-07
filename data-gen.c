3c98f03e1958af617f24c8c7f3ee37e4db0770ff
Kaiming Ouyang <kouya001@ucr.edu>
2021-12-07 14:26:30 -0600

    generate smaller data

--- data-gen.c
+++ data-gen.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_VAL 1073741824
+#define MAX_VAL 4096
+#define MIN_VAL 64
 #define DOUBLE_EPS 1e10
 
 int main(int argc, char* argv[]) {
@@ -14,17 +15,25 @@ int main(int argc, char* argv[]) {
     sprintf(nodes_file, "node-%d-%d.in", nodes, features);
     sprintf(centroids_file, "centroids-%d-%d.in", clusters, features);
 
-    FILE *nodes_fp = fopen(nodes_file, "w");
-    FILE *centroids_fp = fopen(centroids_file, "w");
+    srand(nodes);
+    FILE* nodes_fp = fopen(nodes_file, "w");
+    FILE* centroids_fp = fopen(centroids_file, "w");
 
     for (int i = 0; i < nodes; ++i) {
-        for (int j = 0; j < features; ++j){
-            if (j == features - 1){
-                fprintf(nodes_fp, "%lf\n", (double) (rand() % MAX_VAL) / DOUBLE_EPS + (double) (rand() % MAX_VAL));
-                fprintf(centroids_fp, "%lf\n", (double) (rand() % MAX_VAL) / DOUBLE_EPS + (double) (rand() % MAX_VAL));
+        for (int j = 0; j < features; ++j) {
+            int minus;
+            if (j == features - 1) {
+                minus = (rand() % 2) == 0 ? -1 : 1;
+                fprintf(nodes_fp, "%lf\n", ((double)(rand() % MAX_VAL) / DOUBLE_EPS + (double)(rand() % MIN_VAL)) * minus);
+
+                minus = (rand() % 2) == 0 ? -1 : 1;
+                fprintf(centroids_fp, "%lf\n", ((double)(rand() % MAX_VAL) / DOUBLE_EPS + (double)(rand() % MIN_VAL)) * minus);
             } else {
-                fprintf(nodes_fp, "%lf,", (double) (rand() % MAX_VAL) / DOUBLE_EPS + (double) (rand() % MAX_VAL));
-                fprintf(centroids_fp, "%lf,", (double) (rand() % MAX_VAL) / DOUBLE_EPS + (double) (rand() % MAX_VAL));
+                minus = (rand() % 2) == 0 ? -1 : 1;
+                fprintf(nodes_fp, "%lf,", ((double)(rand() % MAX_VAL) / DOUBLE_EPS + (double)(rand() % MIN_VAL)) * minus);
+
+                minus = (rand() % 2) == 0 ? -1 : 1;
+                fprintf(centroids_fp, "%lf,", ((double)(rand() % MAX_VAL) / DOUBLE_EPS + (double)(rand() % MIN_VAL)) * minus);
             }
         }
     }
